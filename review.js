2d19a5490fe94e555db54de342becf94532ab8de
Raghavkhattar-1504 <raghavkhattar0@gmail.com>
2024-12-10 11:42:08 +0530

    [Raghav]: ADD - week 1 review

---
+++ review.js
@@ -0,0 +1,105 @@
+// let arr = ["raghav", "Sparsh", "Rakshit", "suraj", "rahul"];
+
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+
+// console.log();
+
+
+// arr.forEach(element => {
+//     console.log(element);
+// });
+
+// console.log();
+
+// let i = 0;
+// while (arr.length > i) {
+//     console.log(arr[i]);
+//     i++;
+// }
+
+// console.log();
+
+// let j = 0;
+// do {
+//     console.log(arr[j]);
+//     j++;
+// } while (arr.length > j);
+
+// console.log();
+
+// for (const k in arr) {
+//     console.log(arr[k]);
+// }
+
+// console.log();
+
+// for (const idx of arr) {
+//     console.log(idx);
+// }
+
+// console.log();
+
+
+// const mapify = arr.map(x => x.toUpperCase());
+// console.log(mapify);
+
+// const filterify = arr.filter(x => x.length >= 6);
+// console.log(filterify);
+
+// const reducify = arr.reduce((acc, curr) => {
+//     if (curr.length >= 6) {
+//         acc++;
+//     }
+//     return acc;
+// }, 0);
+// console.log(reducify);
+
+
+// const num = [0, 24, 54, 35, 13];
+
+// const multiply = num.map(x => x );
+// console.log(multiply);
+
+// const prime = num.filter(x => isprime(x));
+// console.log(prime);
+
+// function isprime(n) {
+//     if(n===0 || n===1)return false;
+
+//     for (let z = 2; z <= n / 2; z++) {
+//         if (n % z === 0) {
+//             return false;
+//         }
+//     }
+//     return true;
+// }
+
+
+console.log(12 === 13);
+console.log(13 !== 12);
+console.log(12 >= 14);
+console.log(22 <= 23);
+console.log(1 || 1);
+console.log(1 ?? 0);
+console.log(1 && 1);
+console.log(1 ^ 2);
+console.log(true ? 1 : 0);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
