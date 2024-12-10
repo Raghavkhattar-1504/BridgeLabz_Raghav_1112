2bdc55e4ddc2a05067bd71efe8c6cba793717948
Raghavkhattar-1504 <raghavkhattar0@gmail.com>
2024-12-10 11:34:34 +0530

    [Raghav]: REFACTOR - spread practice

--- spread.js
+++ spread.js
@@ -4,3 +4,10 @@ console.log(slice);
 const spread = [...arr];
 console.log(spread);
 
+
+const arr1 = [1,2,3,4,5];
+const arr2= [ one, two , ,...others] = arr1;
+arr2.push(77);
+console.log(others);
+console.log(arr2[3]);
+
