bc48945d03a461143a0c4ecd418821efd92afabd
Raghavkhattar-1504 <raghavkhattar0@gmail.com>
2024-12-11 14:44:35 +0530

    [Raghav]: ADD - function to add two arrays with unique numbers

--- 25_concatarray.js
+++ 25_concatarray.js
@@ -1,8 +1,19 @@
-const arr1 = [1,2,3,4,5];
-const arr2 = [6,7,8,9,0];
+const arr1 = [1,2,3,9,5];
+const arr2 = [6,7,3,9,0];
 
 const con = (arr1,arr2) => {
-    return arr1.concat(arr2);
+   const set = new Set(arr1);
+   for (let i = 0; i < arr2.length; i++) {
+    if (set.has(arr2[i])) {
+        set.delete(arr2[i]);
+    }
+    else{
+        set.add(arr2[i]);
+       }
+   }
+   const ans = [...set];
+   return ans;
 }
+
 
 console.log(con(arr1,arr2));
