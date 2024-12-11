6d814d10b08b03b36004d7f2f59059c0afe9d64c
Raghavkhattar-1504 <raghavkhattar0@gmail.com>
2024-12-11 15:12:27 +0530

    [Raghav]: REFACTOR - modify the code for only 100 values

--- 18_100prime.js
+++ 18_100prime.js
@@ -3,7 +3,7 @@ import {prime} from "./16_prime.js";
 let cnt = 0;
 let int = 2;
 let arr = [];
-while (cnt <= 100) {
+while (cnt < 100) {
     if (prime(int)) {
         arr.push(int);
         cnt++;
