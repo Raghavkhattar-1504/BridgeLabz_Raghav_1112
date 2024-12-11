d9f01fcc209dab8dca11b9f784675e17dd40eb34
Raghavkhattar-1504 <raghavkhattar0@gmail.com>
2024-12-11 14:49:56 +0530

    [Raghav]: ADD - function to return array with umique number s of array 1

---
+++ 26_uniquearray.js
@@ -0,0 +1,17 @@
+const arr1 = [1,2,3,9,5];
+const arr2 = [6,7,3,9,0];
+
+const con = (arr1,arr2) => {
+   const set = new Set(arr1);
+   for (let i = 0; i < arr2.length; i++) {
+    if (set.has(arr2[i])) {
+        set.delete(arr2[i]);
+    }
+   
+   }
+   const ans = [...set];
+   return ans;
+}
+
+
+console.log(con(arr1,arr2));
