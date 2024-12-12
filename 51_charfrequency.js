882ff96057daae700b096acd38718a6e6e0c114a
Raghavkhattar-1504 <raghavkhattar0@gmail.com>
2024-12-12 13:52:51 +0530

    [Raghav] : ADD - Frequency array of charcters

---
+++ 51_charfrequency.js
@@ -0,0 +1,18 @@
+const str = "himynameisraghav";
+let map = new Map();
+for (let i = 0; i < str.length; i++) {
+    if(map.has(str[i])){
+        map.set(str[i], map.get(str[i])+1);
+    }
+    else{
+        map.set(str[i], 1);
+    }
+}
+
+let arr =  [];
+for ([key, value]  of map.entries()) {
+    arr.push([key,value]);
+}
+
+console.log(arr);
+
