7be179dc6b998dad7ae0feadbff03104ac7155e3
Raghavkhattar-1504 <raghavkhattar0@gmail.com>
2024-12-12 10:48:07 +0530

    [Raghav] : ADD - Distance between two coordinates

---
+++ 41_distance.js
@@ -0,0 +1,16 @@
+function distance(x1,x2,y1,y2) {
+    xdif = x2-x1;
+    ydif = y2-y1;
+    let ans = Math.sqrt((xdif**2)+(ydif**2));
+    return ans;
+}
+
+let x1 = 5;
+let x2 = 7;
+let y1 = 13;
+let y2 = 52;
+
+const sum = distance(x1,x2,y1,y2).toFixed(2);
+console.log(sum);
+
+
