1996e1d8bb01b63bb1f759fbf98bce9a05fc2d68
Raghavkhattar-1504 <raghavkhattar0@gmail.com>
2024-12-12 14:07:27 +0530

    [Raghav] : REFACTOR - Modify the function using DP

--- 15_nthfibonacci.js
+++ 15_nthfibonacci.js
@@ -1,11 +1,16 @@
-const nfib = (n) => {
+let dp = Array(501).fill(-1);
+export const nfib = (n, dp) => {
     if (n === 1) {
         return 1;
     }
     if (n === 0) {
         return 0;
     }
-    return nfib(n - 1) + nfib(n - 2);
+    if(dp[n] !== -1){
+        return dp[n];
+    }
+    // console.log(n);
+    return dp[n]=BigInt(nfib(n - 1, dp)) + BigInt(nfib(n - 2, dp));
 }
 
-console.log(nfib(5));
+// console.log(nfib(5,dp));
