03eebe933959af0a222f213a8f9f89bd5ba1ba04
Raghavkhattar-1504 <raghavkhattar0@gmail.com>
2024-12-12 10:40:31 +0530

    [Raghav] : ADD - Bubble sort function

---
+++ 40_bubblesort.js
@@ -0,0 +1,19 @@
+const arr = [13,14,-14,52,56,53];
+
+function bubble(arr) {
+    let swapped ;
+    do {
+        swapped  = false;
+        for (let i = 0; i < arr.length-1; i++) {
+            if(arr[i] > arr[i+1]){
+                let temp = arr[i];
+                arr[i] = arr[i+1];
+                arr[i+1] = temp;
+                swapped = true;
+            }
+        }
+
+    } while (swapped);
+}
+bubble(arr);
+console.log(arr);
