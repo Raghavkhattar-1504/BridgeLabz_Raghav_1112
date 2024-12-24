9bba13f18ca8cd18ec09da2e08458d02526f7f0e
Raghavkhattar-1504 <raghavkhattar0@gmail.com>
2024-12-24 13:50:17 +0530

    [Raghav]: REMOVE - Array files directory change

--- arr5.js
+++
@@ -1,6 +0,0 @@
-let arr = [1,2,3,4,6,1];
-for (let i = 0; i < arr.length; i++) {
-    if (!arr.includes(i+1)) {
-        console.log(i+1);   
-    }
-}
--- arr8.js
+++
@@ -1,3 +0,0 @@
-let arr = [12,52,72,21,56];
-arr.sort((a,b) => a-b);
-console.log(arr[arr.length-1]*arr[arr.length-2]*arr[arr.length-3]);
