7c060e170a295f6d645f8d38713221e9f8cc9f74
Raghavkhattar-1504 <raghavkhattar0@gmail.com>
2025-01-06 23:59:16 +0530

    [Raghav]: ADD - Single Responsibilty Principle

---
+++ SolidProps/singleton.js
@@ -0,0 +1,12 @@
+class UserSaver {
+    save(user) {
+      // Save user to database
+    }
+  }
+  
+  class EmailService {
+    sendWelcomeEmail(user) {
+      // Send a welcome email
+    }
+  }
+  
