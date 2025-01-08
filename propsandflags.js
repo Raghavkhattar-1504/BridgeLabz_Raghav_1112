2adf75c11b2dc6994a0d44686fb5c13790c1be89
Raghavkhattar-1504 <raghavkhattar0@gmail.com>
2025-01-08 13:01:11 +0530

    [Raghav]: ADD - Objet properties and references practice

---
+++ propsandflags.js
@@ -0,0 +1,15 @@
+const obj = {};
+Object.defineProperty(obj, "age", {
+  value: 25,
+  writable: false,
+  enumerable: true,
+  configurable: true,
+});
+
+console.log(obj.age); 
+
+
+console.log(a);
+const a = 10;
+console.log(a);
+
