21472eebac4c3e9b435688aefd3f8088421bb542
Raghavkhattar-1504 <raghavkhattar0@gmail.com>
2025-01-08 13:35:14 +0530

    [Raghav]: ADD - Property flags and descriptors  practice

--- propsandflags.js
+++ propsandflags.js
@@ -13,3 +13,17 @@ console.log(a);
 const a = 10;
 console.log(a);
 
+
+
+const obj = {};
+
+Object.defineProperty(obj, "id", {
+  value: 101,
+  configurable: false, 
+});
+
+console.log(obj.id); 
+
+delete obj.id;       
+console.log(obj.id); 
+
