387ad64f2d8bd056a38c07ca51c4cdb4535b9937
Raghavkhattar-1504 <raghavkhattar0@gmail.com>
2025-01-10 08:52:53 +0530

    [Raghav]: REFACTOR - Object properties and flags practice

--- propsandflags.js
+++ propsandflags.js
@@ -1,29 +1,87 @@
-const obj = {};
-Object.defineProperty(obj, "age", {
-  value: 25,
-  writable: false,
-  enumerable: true,
-  configurable: true,
-});
 
-console.log(obj.age); 
+const car = {
+  make: "Maruti",
+  model: "800",
+  year: 2010,
+};
 
 
-console.log(a);
-const a = 10;
-console.log(a);
-
-
-
-const obj = {};
-
-Object.defineProperty(obj, "id", {
-  value: 101,
+Object.defineProperty(car, "make", {
+  writable: false, 
+  enumerable: true, 
   configurable: false, 
 });
 
-console.log(obj.id); 
+Object.defineProperty(car, "year", {
+  writable: true,
+  enumerable: false,
+  configurable: true, 
+});
 
-delete obj.id;       
-console.log(obj.id); 
+console.log(Object.getOwnPropertyDescriptor(car, "make")); 
+console.log(Object.getOwnPropertyDescriptor(car, "year")); 
+
+car.make = "BMW"; 
+console.log(car.make); 
+
+console.log(Object.keys(car)); 
+console.log(Object.values(car));
+
+
+
+const person = {};
+
+Object.defineProperties(person, {
+  name: { value: "raghav",
+     writable: true,
+      enumerable: true,
+       configurable: true 
+      },
+  age: { value: 21,
+     writable: false, 
+     enumerable: true, 
+     configurable: true 
+    },
+  address: { value: { city: "rajpura", pinCode: "140401" },
+   writable: true, 
+   enumerable: false, 
+   configurable: true 
+  },
+});
+
+Object.defineProperties(person.address, {
+  city: { value: "rajpura",
+     writable: true, 
+     enumerable: true, 
+     configurable: true 
+    },
+  pinCode: { value: "140401",
+     writable: false, 
+     enumerable: true, 
+     configurable: false 
+    },
+});
+
+console.log(person.name);
+console.log(person.age);
+console.log(person.address);
+
+console.log(Object.keys(person));
+
+person.name = "Sparsh";
+console.log(person.name);
+
+person.age = 35;
+console.log(person.age);
+
+person.address.city = "patiala";
+console.log(person.address.city);
+
+person.address.zipCode = "110041";
+console.log(person.address.pinCode);
+
+delete person.address.pinCode;
+console.log(person.address.pinCode);
+
+
 
