4bc40bdab3a2e6b92a65afe83c10e2e6a3ee9001
Raghavkhattar-1504 <raghavkhattar0@gmail.com>
2025-01-10 08:53:16 +0530

    [Raghav]: REFACTOR - chaining practice

--- chaining.js
+++ chaining.js
@@ -16,5 +16,56 @@ const user = {
   
   console.log(person.greet?.());  
   console.log(person.farewell?.()); 
+
+
+console.log();
+
+
+
+  const obj1 = {
+    name: "raghav",
+    address: {
+      city: "rajpura",
+      pin: "140401",
+      course: {
+        college : "chitkara",
+        branch : "cse"
+      },
+    },
+  };
+
+  console.log(obj1.address?.course?.college);
+  console.log(obj1.address?.course?.year); 
+  
+  console.log(obj1.contact?.email); 
+
+
+
+  const users = [
+    { name: "raghav", age: 21 },
+    { name: "ram" }, 
+    { name: "shyam", age: 24 },
+  ];
+  
+  users.forEach((user, index) => {
+    console.log(`${user.name},  ${user.age ?? "Not provided"}`);
+  });
+  
+
+
+
+  const name = {
+    fullname: () => "Raghav Khattar",
+    age: undefined, 
+  };
+
+  console.log(name.fullname?.()); 
+  
+
+  console.log(name.age?.() ?? "fals"); 
+  
+  
+  
+  
   
   
